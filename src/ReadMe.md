507ce332574e4f83839760197cc044eec614f2e7
MajidJowDev <majid.jow@gmail.com>
2023-04-01 02:56:21 +0330

    initiate commit

---
+++ src/ReadMe.md
@@ -0,0 +1,2 @@
+# RestAPI - CRUD Operations
+A simple RestAPI Application with built-in/in-memory databases to demonstrate CRUD Operation
---
+++ src/test/java/mjz/restapi/crudapi/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mjz.restapi.crudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
